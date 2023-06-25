bbad833b57831fba6381f040570408ec6dee60c1
james2002chen <james2002chen@126.com>
2023-06-25 23:26:36 +0800

    update 12 files

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,7 +3,8 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
+use std::f32::consts::E;
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -15,6 +16,10 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    if num==0 {1}
+    else{
+        (1..=num).fold(1, |acc, x| acc*x)
+    }
 }
 
 #[cfg(test)]
